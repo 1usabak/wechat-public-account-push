3cb9a03ef4088ac473e12bb1848106439287d326
1usabak <94665968+1usabak@users.noreply.github.com>
2022-11-14 18:54:33 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -14,16 +14,16 @@ const USER_CONFIG = {
   APP_SECRET: '4170308b6b8a934666fd17c0aebbe540',
 
   PROVINCE: '湖北',
-  CITY: '黄石',
+  CITY: '十堰',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '小夏',
+      name: '小徐',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '4170308b6b8a934666fd17c0aebbe540',
+      id: 'orGaO6UR-WzOvPH696amwnN6ESUc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ISea_WvDsbwxHaYdoNLCReoj0Xu9b9rD3l-nyH3YbA0',
+      useTemplateId: 'QrXPA0MtCInHRaUcVOvzTRJwy6DnsW9FaB-rUIdOaOs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'orGaO6UR-WzOvPH696amwnN6ESUc',
     }
   ],
 
