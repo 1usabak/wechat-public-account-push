9644418586570ac1c5210ec58358a60986af316e
1usabak <94665968+1usabak@users.noreply.github.com>
2022-11-14 18:59:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,4 +1,4 @@
-/* eslint-disable */
+ri/* eslint-disable */
 
 /**
  * 此项目配置为方便新人使用，已缩减至最简配置。
@@ -29,14 +29,14 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+         // type: '*生日', name: '宝贝', year: '1996', date: '09-09',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+        //  type: '生日', name: '李四', year: '1996', date: '09-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '解放日', year: '2020', date: '12-24',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
