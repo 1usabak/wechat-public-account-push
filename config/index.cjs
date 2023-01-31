e967da659f9a4eddc40231b3334a378d07714a48
1usabak <94665968+1usabak@users.noreply.github.com>
2023-01-31 12:03:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'QrXPA0MtCInHRaUcVOvzTRJwy6DnsW9FaB-rUIdOaOs',
+  CALLBACK_TEMPLATE_ID: '	ZmIJ41QnE8YAyfVLIEcgCfpAYsmEktHWB4l3tmmE4Q4',
 
   CALLBACK_USERS: [
     {
